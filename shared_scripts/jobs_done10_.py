208d2e353cc439ba470519f62a11b32c08484d38
Diogo de Campos <campos@esss.com.br>
2013-11-19 17:15:23 -0200

    Changed to EsssProject instead of EsssTool (no executables)

--- shared_scripts/jobs_done10_.py
+++ shared_scripts/jobs_done10_.py
@@ -1,12 +1,12 @@
-from sharedscripts10.shared_scripts.esss_application import EsssTool
 from sharedscripts10.namespace.namespace_types import PATHLIST
+from sharedscripts10.shared_scripts.esss_project import EsssProject
 
 
 
 #===================================================================================================
 # JobsDone10
 #===================================================================================================
-class JobsDone10(EsssTool):
+class JobsDone10(EsssProject):
 
     NAME = 'jobs_done10'
 
