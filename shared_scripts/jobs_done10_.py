0fb2d71659f318cde2666c81345e0881d4bf1334
Diogo de Campos <campos@esss.com.br>
2013-12-03 14:57:27 -0200

    Added git dependency

--- shared_scripts/jobs_done10_.py
+++ shared_scripts/jobs_done10_.py
@@ -11,16 +11,19 @@ class JobsDone10(EsssProject):
     NAME = 'jobs_done10'
 
     DEPENDENCIES = [
-        'ben10',
-        'sharedscripts10',
         'jenkins_job_builder',
         'python_jenkins',
+
+        'git', # Used whencreating jobs from local repository
+
+        'ben10',
+        'sharedscripts10',
     ]
 
 
     NAMESPACE_VARIABLES = {
         '$PATH' : PATHLIST('`self.scripts_dir`'),
-        
+
         '>jd_eden' : 'jobs_done jenkins http://eden.fln.esss.com.br:9090/ --username=dev --password=***REMOVED***',
         '>jd_yoda' : 'jobs_done jenkins http://hub.esss.com.br:9090/ --username=dev --password=***REMOVED***',
     }
