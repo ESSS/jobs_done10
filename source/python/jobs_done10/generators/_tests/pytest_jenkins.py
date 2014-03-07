53db85b10d7e17ba0b3d50a257f81b14c6c5cc44
Diogo de Campos <campos@esss.com.br>
2014-03-07 10:24:32 -0300

    Joined jenkins tests in a single module

--- source/python/jobs_done10/generators/_tests/pytest_jenkins.py
+++ source/python/jobs_done10/generators/_tests/pytest_jenkins.py
@@ -1,16 +1,24 @@
+from ben10.filesystem import CreateDirectory, CreateFile, GetFileContents, ListFiles
 from ben10.foundation.string import Dedent
+from jobs_done10.generators.jenkins import (GetJobsFromDirectory, GetJobsFromFile, JenkinsJob,
+    JenkinsJobPublisher, JenkinsXmlJobGenerator, UploadJobsFromFile)
+from jobs_done10.git import Git
 from jobs_done10.job_generator import JobGeneratorConfigurator
-from jobs_done10.jobs_done_job import JobsDoneJob
+from jobs_done10.jobs_done_job import JOBS_DONE_FILENAME, JobsDoneJob
 from jobs_done10.repository import Repository
+import difflib
 import functools
+import jenkins
+import os
 import pytest
+import re
 
 
 
 #===================================================================================================
-# Test
+# TestJenkinsXmlJobGenerator
 #===================================================================================================
-class Test(object):
+class TestJenkinsXmlJobGenerator(object):
 
     #===============================================================================================
     # Setup for common results in all tests
@@ -753,7 +761,6 @@ class Test(object):
         # This test should create two jobs_done_jobs from their variations
         jobs_done_jobs = JobsDoneJob.CreateFromYAML(ci_contents, repository)
 
-        from jobs_done10.generators.jenkins import JenkinsXmlJobGenerator
         job_generator = JenkinsXmlJobGenerator()
         for jd_file in jobs_done_jobs:
             JobGeneratorConfigurator.Configure(job_generator, jd_file)
@@ -850,7 +857,6 @@ class Test(object):
         repository = Repository(url='http://fake.git', branch='not_master')
         jobs_done_jobs = JobsDoneJob.CreateFromYAML(ci_contents, repository)
 
-        from jobs_done10.generators.jenkins import JenkinsXmlJobGenerator
         job_generator = JenkinsXmlJobGenerator()
         JobGeneratorConfigurator.Configure(job_generator, jobs_done_jobs[0])
         jenkins_job = job_generator.GetJobs()
@@ -859,16 +865,202 @@ class Test(object):
 
 
     def _AssertDiff(self, obtained_xml, expected_diff):
-        import difflib
-
         diff = ''.join(difflib.unified_diff(
             self.BASIC_EXPECTED_XML.splitlines(1),
             str(obtained_xml).splitlines(1),
             n=0,
         ))
         diff = '\n'.join(diff.splitlines()[2:])
-        import re
         diff = re.sub('@@.*@@', '@@ @@', diff, flags=re.MULTILINE)
 
         print diff
         assert expected_diff == diff
+
+
+
+#===================================================================================================
+# TestJenkinsActions
+#===================================================================================================
+class TestJenkinsActions(object):
+    '''
+    Integration tests for Jenkins actions
+    '''
+
+    _JOBS_DONE_FILE_CONTENTS = Dedent(
+        '''
+        junit_patterns:
+        - "junit*.xml"
+
+        boosttest_patterns:
+        - "cpptest*.xml"
+
+        parameters:
+          - choice:
+              name: "PARAM"
+              choices:
+              - "choice_1"
+              - "choice_2"
+              description: "Description"
+
+        build_batch_commands:
+        - "command"
+
+        matrix:
+            planet:
+            - mercury
+            - venus
+            - jupiter
+
+        '''
+    )
+
+    _REPOSITORY = Repository(url='http://space.git', branch='branch')
+
+    def testGetJobsFromFile(self, embed_data):
+        job_group, jobs = GetJobsFromFile(self._REPOSITORY, self._JOBS_DONE_FILE_CONTENTS)
+
+        assert job_group == self._REPOSITORY.name + '-' + self._REPOSITORY.branch
+        assert len(jobs) == 3
+
+
+    def testGetJobsFromDirectory(self, embed_data):
+        repo_path = embed_data['git_repository']
+        CreateDirectory(repo_path)
+
+        # Prepare git repository
+        git = Git()
+        git.Execute(['init'], repo_path)
+        git.AddRemote(repo_path, 'origin', self._REPOSITORY.url)
+        git.CreateLocalBranch(repo_path, self._REPOSITORY.branch)
+        CreateFile(os.path.join(repo_path, '.gitignore'), '')
+        git.Add(repo_path, '.')
+        git.Commit(repo_path, 'First commitAdded jobs_done file')
+
+        # If there is no jobs_done file, we should get zero jobs
+        job_group, jobs = GetJobsFromDirectory(repo_path)
+        assert job_group == 'space-branch'
+        assert len(jobs) == 0
+
+        # Create jobs_done file
+        CreateFile(os.path.join(repo_path, JOBS_DONE_FILENAME), self._JOBS_DONE_FILE_CONTENTS)
+        git.Add(repo_path, '.')
+        git.Commit(repo_path, 'Added jobs_done file')
+
+        job_group, jobs = GetJobsFromDirectory(repo_path)
+        assert job_group == self._REPOSITORY.name + '-' + self._REPOSITORY.branch
+        assert len(jobs) == 3
+
+
+    def testUploadJobsFromFile(self, monkeypatch):
+        '''
+        Tests that UploadJobsFromFile correctly calls JenkinsJobPublisher (already tested elsewhere)
+        '''
+        def MockPublishToUrl(self, url, username, password):
+            assert url == 'jenkins_url'
+            assert username == 'jenkins_user'
+            assert password == 'jenkins_pass'
+
+            assert set(self.jobs.keys()) == set([
+                'space-branch-venus', 'space-branch-jupiter', 'space-branch-mercury'])
+
+            return 'mock publish result'
+
+        monkeypatch.setattr(JenkinsJobPublisher, 'PublishToUrl', MockPublishToUrl)
+
+        result = UploadJobsFromFile(
+            repository=self._REPOSITORY,
+            jobs_done_file_contents=self._JOBS_DONE_FILE_CONTENTS,
+            url='jenkins_url',
+            username='jenkins_user',
+            password='jenkins_pass',
+        )
+        assert result == 'mock publish result'
+
+
+
+#===================================================================================================
+# TestJenkinsPublisher
+#===================================================================================================
+class TestJenkinsPublisher(object):
+
+    def testPublishToDirectory(self, embed_data):
+        self._GetPublisher().PublishToDirectory(embed_data['.'])
+
+        assert set(ListFiles(embed_data['.'])) == set([
+            'space-milky_way-jupiter', 'space-milky_way-mercury', 'space-milky_way-venus'])
+
+        assert GetFileContents(embed_data['space-milky_way-jupiter']) == 'jupiter'
+        assert GetFileContents(embed_data['space-milky_way-mercury']) == 'mercury'
+        assert GetFileContents(embed_data['space-milky_way-venus']) == 'venus'
+
+
+    def testPublishToUrl(self, monkeypatch):
+        mock_jenkins = self._MockJenkinsAPI(monkeypatch)
+
+        new_jobs, updated_jobs, deleted_jobs = self._GetPublisher().PublishToUrl(
+            url='jenkins_url',
+            username='jenkins_user',
+            password='jenkins_pass',
+        )
+        assert set(new_jobs) == mock_jenkins.NEW_JOBS == set(['space-milky_way-venus', 'space-milky_way-jupiter'])
+        assert set(updated_jobs) == mock_jenkins.UPDATED_JOBS == set(['space-milky_way-mercury'])
+        assert set(deleted_jobs) == mock_jenkins.DELETED_JOBS == set(['space-milky_way-saturn'])
+
+
+    def testPublishToUrl2(self, monkeypatch):
+        mock_jenkins = self._MockJenkinsAPI(monkeypatch)
+
+        publisher = self._GetPublisher()
+        publisher.jobs = {}
+
+        new_jobs, updated_jobs, deleted_jobs = publisher.PublishToUrl(
+            url='jenkins_url',
+            username='jenkins_user',
+            password='jenkins_pass',
+        )
+        assert set(new_jobs) == mock_jenkins.NEW_JOBS == set()
+        assert set(updated_jobs) == mock_jenkins.UPDATED_JOBS == set()
+        assert set(deleted_jobs) == mock_jenkins.DELETED_JOBS == set(['space-milky_way-mercury', 'space-milky_way-saturn'])
+
+
+    def _GetPublisher(self):
+        job_group = 'space-milky_way'
+        jobs = [
+            JenkinsJob(name='space-milky_way-jupiter', xml='jupiter'),
+            JenkinsJob(name='space-milky_way-mercury', xml='mercury'),
+            JenkinsJob(name='space-milky_way-venus', xml='venus'),
+        ]
+
+        return JenkinsJobPublisher(job_group, jobs)
+
+
+    def _MockJenkinsAPI(self, monkeypatch):
+        class MockJenkins(object):
+            NEW_JOBS = set()
+            UPDATED_JOBS = set()
+            DELETED_JOBS = set()
+
+            def __init__(self, url, username, password):
+                assert url == 'jenkins_url'
+                assert username == 'jenkins_user'
+                assert password == 'jenkins_pass'
+
+            def get_jobs(self):
+                return [
+                    {'name' : u'space-milky_way-mercury'},
+                    {'name' : u'space-milky_way-saturn'},
+                ]
+
+            def create_job(self, name, xml):
+                self.NEW_JOBS.add(name)
+
+            def reconfig_job(self, name, xml):
+                self.UPDATED_JOBS.add(name)
+
+            def delete_job(self, name):
+                self.DELETED_JOBS.add(name)
+
+
+        monkeypatch.setattr(jenkins, 'Jenkins', MockJenkins)
+
+        return MockJenkins
