73bc48ebf9f67266c8ac77a13c94e2e0a30aedbd
Bruno Oliveira <bruno@esss.com.br>
2016-01-28 08:46:39 -0200

    Remove obsolete and unused files
    EDEN-1537

--- coverage.bat
+++
@@ -1,6 +0,0 @@
-set COVERAGE_MODULE=jobs_done10.log
-set COVERAGE_PATH=source\python\jobs_done10\_tests\pytest_log.py
-set COVERAGE_MODULE=jobs_done10
-set COVERAGE_PATH=source\python\jobs_done10
-pytest --cov-report term-missing --cov %COVERAGE_MODULE% %COVERAGE_PATH%
-
