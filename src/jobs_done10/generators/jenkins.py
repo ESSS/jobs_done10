be3586a426eb1290ea0af5ecab080f97791ca1d9
Tiago de Holanda Cunha Nobrega <tnobrega@esss.com.br>
2018-12-03 11:18:48 -0200

    Account for remote url notation difference
    Since 'remote.origin.url' for the main repository is set on initial
    cloning and NOT on jobs_done.yml, it's possible to have a remote url that
    refers to 'ssh://example.com/repo.git' while git's remote.origin.url
    refers to 'ssh://example.com/repo' (which is the same repo). This caused
    the job creation to fail because we do basic string comparison.
    
    EDEN-1873

--- src/jobs_done10/generators/jenkins.py
+++ src/jobs_done10/generators/jenkins.py
@@ -758,12 +758,19 @@ class JenkinsJobPublisher(object):
         scms = root.findall('scm/scms/hudson.plugins.git.GitSCM')
 
         checked_urls = []
+
+        # Account for case-differences
+        repo_lower = self.repository.url.lower()
+        # Account for the fact that ssh://example.com/repo and ssh://example.com/repo.git
+        # refer to the same repo
+        repo_lower_git = repo_lower + '.git'
+
         # Process them all until we find the SCM for the correct repository
         for scm in scms:
             url = scm.find('userRemoteConfigs/hudson.plugins.git.UserRemoteConfig/url').text.strip()
             checked_urls.append(url)
 
-            if url == self.repository.url:
+            if url.lower() in (repo_lower, repo_lower_git):
                 return scm.find('branches/hudson.plugins.git.BranchSpec/name').text.strip()
 
         error_msg = [
