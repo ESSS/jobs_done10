68b565f69a8818b9a6c0ab5dd5e705acf3d9b313
Igor Ghisi <igor@esss.co>
2019-09-11 15:09:23 -0300

    Add Docker build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+if [ $# -eq 0 ]
+  then
+    echo "Version not given"
+    exit 1
+fi
+
+set IMAGE_NAME=docker.esss.co/jobs_done
+
+docker build . --tag $IMAGE_NAME:$1 --build-arg SETUPTOOLS_SCM_PRETEND_VERSION=$1
+docker tag $IMAGE_NAME:$1 $IMAGE_NAME:latest
+docker push $IMAGE_NAME:$1
+docker push $IMAGE_NAME:latest
